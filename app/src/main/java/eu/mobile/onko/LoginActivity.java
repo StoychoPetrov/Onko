2b1102f49ba0cf0d187e8acd69ef24ecd26abb8e
StoychoPetrov <StoychoPetrov95@gmail.com>
2017-09-04 15:58:15 +0300

    Change Login.

--- app/src/main/java/eu/mobile/onko/LoginActivity.java
+++ app/src/main/java/eu/mobile/onko/LoginActivity.java
@@ -2,12 +2,26 @@ package eu.mobile.onko;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText    mPasswordEdt;
+    private EditText    mEmailEdt;
+    private Button      mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initUI();
+    }
+
+    private void initUI(){
+        mEmailEdt       = (EditText)    findViewById(R.id.email_edt);
+        mPasswordEdt    = (EditText)    findViewById(R.id.password_edt);
+        mLoginBtn       = (Button)      findViewById(R.id.login_btn);
     }
 }
